c8c29e5cbb60a81a82bd4aa32920c88cf22a75da
Kostadin Gramatikov <kgramatikov993@gmail.com>
2024-03-06 11:56:28 +0200

    Uploading Factroy and Abstract factory desing patterns

---
+++ Abstract Factory/AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+abstract class Weapon
+{
+    public abstract void Attack();
+}
+
+// Concrete products for Weapon Class
+class Bow : Weapon
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Attacking with Bow!");
+    }
+}
+
+class Axe : Weapon
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Attacking with Axe!");
+    }
+}
+
+// Abstract product B
+abstract class Armor
+{
+    public abstract void Defend();
+}
+
+// Concrete products for Armor class
+class LeatherArmor : Armor
+{
+    public override void Defend()
+    {
+        Console.WriteLine("Defending with Leather Armor!");
+    }
+}
+
+class PlateArmor : Armor
+{
+    public override void Defend()
+    {
+        Console.WriteLine("Defending with Plate Armor!");
+    }
+}
+
+// Abstract factory
+abstract class EquipmentFactory
+{
+    public abstract Weapon CreateWeapon();
+    public abstract Armor CreateArmor();
+}
+
+// Concrete factory for creating fantasy equipment
+class FantasyEquipmentFactory : EquipmentFactory
+{
+    public override Weapon CreateWeapon()
+    {
+        return new Bow();
+    }
+
+    public override Armor CreateArmor()
+    {
+        return new LeatherArmor();
+    }
+}
+
+// Concrete factory for creating medieval equipment
+class MedievalEquipmentFactory : EquipmentFactory
+{
+    public override Weapon CreateWeapon()
+    {
+        return new Axe();
+    }
+
+    public override Armor CreateArmor()
+    {
+        return new PlateArmor();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a fantasy equipment factory
+        EquipmentFactory fantasyFactory = new FantasyEquipmentFactory();
+        // Create fantasy weapon and armor
+        Weapon fantasyWeapon = fantasyFactory.CreateWeapon();
+        Armor fantasyArmor = fantasyFactory.CreateArmor();
+        // Use fantasy equipment
+        fantasyWeapon.Attack();
+        fantasyArmor.Defend();
+
+        Console.WriteLine("------------------------------");
+
+        // Create a medieval equipment factory
+        EquipmentFactory medievalFactory = new MedievalEquipmentFactory();
+        // Create medieval weapon and armor
+        Weapon medievalWeapon = medievalFactory.CreateWeapon();
+        Armor medievalArmor = medievalFactory.CreateArmor();
+        // Use medieval equipment
+        medievalWeapon.Attack();
+        medievalArmor.Defend();
+
+        Console.ReadKey();
+    }
+}
---
+++ Factory Desing/Factory Method/Factory Method/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Abstract class representing an enemy
+abstract class Enemy
+{
+    public abstract void Attack();
+}
+
+// Concrete enemy classes
+class Goblin : Enemy
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Goblin attacks with a club!");
+    }
+}
+
+class Skeleton : Enemy
+{
+    public override void Attack()
+    {
+        Console.WriteLine("Skeleton attacks with a sword!");
+    }
+}
+
+// Abstract factory class for creating enemies
+abstract class EnemySpawner
+{
+    public abstract Enemy CreateEnemy();
+}
+
+// Concrete factory classes for creating specific types of enemies
+class GoblinSpawner : EnemySpawner
+{
+    public override Enemy CreateEnemy()
+    {
+        return new Goblin();
+    }
+}
+
+class SkeletonSpawner : EnemySpawner
+{
+    public override Enemy CreateEnemy()
+    {
+        return new Skeleton();
+    }
+}
+
+class MainApp
+{
+    static void Main()
+    {
+        // An array of enemy
+        EnemySpawner[] enemyFactories = new EnemySpawner[2];
+        enemyFactories[0] = new GoblinSpawner();
+        enemyFactories[1] = new SkeletonSpawner();
+
+        foreach (EnemySpawner factory in enemyFactories)
+        {
+            Enemy enemy = factory.CreateEnemy();
+            enemy.Attack();
+        }
+
+        Console.ReadKey();
+    }
+}
